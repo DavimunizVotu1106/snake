9dae2bd8a9856a1fe11f5df55c2b4da012c8d6e8
DaviM1106 <davi.m.santos9@edu.senai.br>
2026-08-31 12:53:28 -0300

    Add snake movement logic

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ ctx.shadowBlur = 10;
 ctx.fillStyle = "#39ff14";
 
 ctx.fillRect(snake[0].x, snake[0].y, 20, 20);
+ctx.fillRect(snake[1].x, snake[1].y, 20, 20);
+
+
+for (let i = 0; i < snake.length; i++) {
+
+    ctx.fillRect(snake[i].x, snake[i].y, 20, 20);
+}
+
+let direction = "right";
+    if (direction === "right") {
+        snake[0].x +=10;
+}
+
+setInterval(() => {
+    if (direction === "right") {
+        snake[0].x += 10;
+
+        }
+        
+    }, 100);
