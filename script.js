6992b7e9bea2466c34fce600b459ddf8996bb3ad
DaviM1106 <davi.m.santos9@edu.senai.br>
2026-09-01 01:16:29 -0300

    Implement snake movement

--- script.js
+++ script.js
@@ -10,24 +10,29 @@ ctx.shadowBlur = 10;
 
 ctx.fillStyle = "#39ff14";
 
-ctx.fillRect(snake[0].x, snake[0].y, 20, 20);
-ctx.fillRect(snake[1].x, snake[1].y, 20, 20);
-
-
 for (let i = 0; i < snake.length; i++) {
 
     ctx.fillRect(snake[i].x, snake[i].y, 20, 20);
 }
 
 let direction = "right";
-    if (direction === "right") {
-        snake[0].x +=10;
-}
+
 
 setInterval(() => {
+
+    ctx.clearRect(0, 0, 600, 600);
+
+    const oldHeadx = snake[0].x;
+
+
+
     if (direction === "right") {
         snake[0].x += 10;
+        snake[1].x = oldHeadx;
 
+    }
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillRect(snake[i].x, snake[i].y, 20, 20);
         }
         
     }, 100);
